e7c8653722d78aa47020014161b56d5603f2ca62
Amar9827 <44750216+Amar9827@users.noreply.github.com>
2019-09-21 08:00:21 +0530

    Render method
    To render the changes when enter button pressed or while adding an item.

--- index.js
+++ index.js
@@ -30,4 +30,15 @@ class TodoList {
         });
         this.render();
     }
+  template(item, id) {
+        return (`<li class="todo-item ${(item.checked ? "checked" : "")}" data-key="${id}"><input type="checkbox" data-key="${id}" ${(item.checked ? "checked" : "")}/> ${ item.text }</li>`);
+    }
+    render() {
+        let todoElements = [];
+        this.todos.forEach((item, key) => {
+            todoElements.push(this.template(item, key))
+        });
+
+        this.todoList.innerHTML = todoElements.join(" ")
+    }
 }
