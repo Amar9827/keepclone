5e9a307c2646a66e6b5403ba7f90394afdb831ab
Amar9827 <44750216+Amar9827@users.noreply.github.com>
2019-09-21 07:52:41 +0530

    Creating the JavaScript file
    We add a constructor.

---
+++ index.js
@@ -0,0 +1,10 @@
+class TodoList {
+  constructor() {
+        this.todos = new Map();
+        this.todoContainer = document.querySelector(".todo-body");
+        this.todoList = document.querySelector(".todo-list");
+        this.todoInput = document.querySelector(".todoInput");
+        this.removeButton = document.querySelector(".removeText");
+        this.bindEvents();
+    }
+}
